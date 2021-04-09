8148d75219c075b7a5492e9424b0ecf9f70504c2
ismail erggab <ismailerggab@gmail.com>
2021-04-09 11:12:10 +0100

    workshop2

--- javascript/Timeline/script.js
+++ javascript/Timeline/script.js
@@ -1,1 +1,31 @@
-// Votre code javascript
+
+
+  // define variable
+  var move = document.querySelectorAll(".timeline li");
+
+
+  function element(e) {
+    var div = e.getBoundingClientRect();
+    return (
+      div.bottom <= window.innerHeight  &&
+      div.right <= window.innerWidth 
+    );
+  }
+
+  function call() {
+    for (var i = 0; i < move.length; i++) {
+      if (element(move[i])) {
+        move[i].classList.add("show");
+      }
+    }
+  }
+
+
+  document.addEventListener("scroll", call);
+
+
+
+
+
+
+
